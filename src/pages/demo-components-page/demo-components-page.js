94634f697221dfbfb8243e21438330c5ff7c50be
Gregory <lobster287@gmail.com>
2018-08-03 16:49:48 +0700

    Fixing: import JS, TS scripts to the project

--- src/pages/demo-components-page/demo-components-page.js
+++ src/pages/demo-components-page/demo-components-page.js
@@ -5,7 +5,27 @@ function importStyles(context){
     context.keys().forEach(key => context(key));
 }
 
+function importScripts(context){
+    const scripts = {};
+    context.keys().forEach(key => {
+        scripts[key] = context(key)
+    });
+    return scripts;
+}
+
 importStyles(require.context('../../components/', true, /^\.\/.*\.styl$/));
+const scripts = importScripts(
+    require.context('../../components/', true, /^\.\/.*\.(tsx|ts|js)$/)
+);
+
+$(document).ready(() => {
+    for (let key in scripts)
+    {
+        if (scripts[key].default) {
+            scripts[key].default(true);
+        }
+    }
+});
 
 if(module.hot){
     module.hot.accept();
